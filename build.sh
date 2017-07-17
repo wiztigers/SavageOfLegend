7872215ebc9f239b3fd117ef8f20afe0bccbc014
wiztigers <regis.witz@free.fr>
2017-07-17 14:14:40 +0200

    enable travis-ci

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+exportdir=$1
 stylesdir="asciidoctor-stylesheet-factory"
 stylename="AgeOfLegend"
 
 cp -R sass ${stylesdir}/
 cd ${stylesdir} && compass compile && cd -
 cp -R fonts ${stylesdir}/stylesheets/
-asciidoctor     core.adoc -a linkcss -a stylesheet=${stylesdir}/stylesheets/${stylename}.css -o index.html
+asciidoctor     core.adoc -a linkcss -a stylesheet=${stylesdir}/stylesheets/${stylename}.css -o ${exportdir}/index.html
