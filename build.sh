beb42eb20252b8062f997f604a589eeac198eda3
wiztigers <regis.witz@free.fr>
2017-07-17 15:36:01 +0200

    add custom fonts and (try to) use 'em

--- build.sh
+++ build.sh
@@ -4,7 +4,17 @@ exportdir=$1
 stylesdir="asciidoctor-stylesheet-factory"
 stylename="AgeOfLegend"
 
+# clone asciidoctor stylesheet (we are dependent on some these)
+git clone https://github.com/asciidoctor/asciidoctor-stylesheet-factory.git ${stylesdir}
+# add our stylesheet to those
 cp -R sass ${stylesdir}/
+# compile our stylesheet (and every other stylesheet)
 cd ${stylesdir} && compass compile && cd -
-#cp -R fonts ${stylesdir}/stylesheets/
-asciidoctor     core.adoc -a stylesheet=${stylesdir}/stylesheets/${stylename}.css -o ${exportdir}/index.html
+# copy fonts directory
+cp -R fonts ${stylesdir}/stylesheets/
+# copy compiled css to export directory
+cp "${stylesdir}/stylesheets/${stylename}.css" "${exportdir}/${stylename}.css"
+# generate HTML
+asciidoctor     core.adoc -a linkcss -a stylesheet="${stylename}.css" -o ${exportdir}/index.html
+# cleanup what was cloned
+rm -rf ${stylesdir}
