d20a73a967492e9e40539667f43146ceff152085
wiztigers <regis.witz@free.fr>
2017-07-18 16:27:15 +0200

    fix build pipeline and generate PDF from last version of website

--- build-pdf.sh
+++ build-pdf.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-exportdir=$1
-
 echo "Generating PDF ..."
-cd ${exportdir}
 xvfb-run --server-args="-screen 0, 1024x768x24" wkhtmltopdf https://wiztigers.github.io/SavageOfLegend SavageOfLegend.pdf
-cd -
 
