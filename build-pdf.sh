46503d16f233c6f7e511ee6a81b3899619291d8a
wiztigers <regis.witz@free.fr>
2017-07-18 17:03:14 +0200

    some additional references

--- build-pdf.sh
+++ build-pdf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Wait 30s for current version to show up ..."
+sleep 30s
 echo "Generating PDF ..."
 xvfb-run --server-args="-screen 0, 1024x768x24" wkhtmltopdf https://wiztigers.github.io/SavageOfLegend SavageOfLegend.pdf
 
