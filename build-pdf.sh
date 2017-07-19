1f6da3a52f4e5b621f528d1dd386bf9de14636fa
wiztigers <regis.witz@free.fr>
2017-07-19 11:50:05 +0200

    use latest version of wkhtmltopdf

--- build-pdf.sh
+++ build-pdf.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-echo "Wait 30s for current version to show up ..."
-sleep 30s
+exportdir=$1
+projectname=$2
+
+# we have to wait 'a few seconds' for current version of gh-pages to show up ...
+# let's hope the time to wget is sufficient !
+
+# get the latest version of wkhtmltopdf and untar it
+wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+tar xf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+
 echo "Generating PDF ..."
-xvfb-run --server-args="-screen 0, 1024x768x24" wkhtmltopdf https://wiztigers.github.io/SavageOfLegend SavageOfLegend.pdf
+./wkhtmltox/bin/wkhtmltopdf --enable-internal-links --enable-external-links https://wiztigers.github.io/${projectname}/ ${exportdir}/${projectname}.pdf
 
